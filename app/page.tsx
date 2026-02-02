504b03be013c7083ea0da17b2c597463ba7612ec
unknown <rnbrosca2003@gmail.com>
2026-02-02 17:30:16 +0200

    .

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   const projects = [
     { 
   image: "./tracker.png", 
-  link: "https://financetracker-iota.vercel.app/", 
+  link: "https://financetracker-one-green.vercel.app/", 
   title: "Finance Tracker", 
   description: "Un tracker financiar full-stack realizat cu MERN, care ajută utilizatorii să își gestioneze veniturile și cheltuielile. Am integrat autentificare securizată cu Clerk și am construit backend-ul cu Node.js, Express și MongoDB, învățând să gestionez eficient datele. Deploy-ul complet m-a ajutat să înțeleg mai bine integrarea dintre frontend și backend." 
 },
